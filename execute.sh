97f37a4b5126c89bc4403bdc28d100e9b72209d5
ChingTsai <ctsai@netdb.cs.nthu.edu.tw>
2016-04-04 00:03:47 +0800

    Add compile and execute script

---
+++ execute.sh
@@ -0,0 +1,9 @@
+# Do not uncomment these lines to directly execute the script
+# Modify the path to fit your need before using this script
+#hdfs dfs -rm -r /user/TA/WordCount/Output/
+#hadoop jar WordCount.jar wordcount.WordCount /user/shared/WordCount/Input /user/TA/WordCount/Output
+#hdfs dfs -cat /user/TA/WordCount/Output/part-*
+
+hdfs dfs -rm -r output
+hadoop jar ${PWD##*/}.jar CloudCompu.hw1.InvertedIndex input output
+hdfs dfs -cat output/part-*
