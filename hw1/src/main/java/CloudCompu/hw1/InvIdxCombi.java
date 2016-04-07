dcb952581336045a2ac16540dd9cef2aa1664085
ctsai <ctsai@netdb.cs.nthu.edu.tw>
2016-04-07 08:31:32 +0800

    Update new type

--- hw1/src/main/java/CloudCompu/hw1/InvIdxCombi.java
+++ hw1/src/main/java/CloudCompu/hw1/InvIdxCombi.java
@@ -7,23 +7,24 @@ import org.apache.hadoop.io.MapWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class InvIdxCombi extends Reducer<Text, IntWritable, Text, MapWritable> {
-	private MapWritable map = new MapWritable();
+public class InvIdxCombi extends Reducer<Text, KeyDetial, Text, KeyDetial> {
+	private KeyDetial kd = new KeyDetial();
 	private Text word = new Text();
 	private Text file = new Text();
 	private IntWritable count = new IntWritable();
-	public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+
+	public void reduce(Text key, Iterable<KeyDetial> values, Context context) throws IOException, InterruptedException {
 		int sum = 0;
-		for (IntWritable val: values) {
-			sum += val.get();
+		for (KeyDetial val : values) {
+			sum += val.getWordCount();
 		}
-		String[] file_word = key.toString().split("_");
-		word.set(file_word[0]);
-		file.set(file_word[1]);
+		String[] fileWord = key.toString().split("_");
+		word.set(fileWord[0]);
+		file.set(fileWord[1]);
 		count.set(sum);
-		
+
 		map.put(file, count);
-		context.write(word,map);
-		
+		context.write(word, map);
+
 	}
 }
--- hw1/src/main/java/CloudCompu/hw1/KeyDetial.java
+++ hw1/src/main/java/CloudCompu/hw1/KeyDetial.java
@@ -3,42 +3,37 @@ package CloudCompu.hw1;
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 
+import org.apache.hadoop.io.MapWritable;
 import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.util.ReflectionUtils;
 
-public class KeyDetial implements Writable {
-	private HashMap<Integer, Integer> wrdMap;
+public class KeyDetial extends MapWritable {
+	private int wordCount = 0;
+	private Map<Writable, Writable> instance;
 
-	public KeyDetial() {
-		wrdMap = new HashMap<Integer, Integer>();
+	public void setWordCount(int count) {
+		this.wordCount = count;
 	}
 
-	public HashMap<Integer, Integer> getTable() {
-		return wrdMap;
+	public int getWordCount() {
+		return this.wordCount;
 	}
 
-	public void readFields(DataInput in) throws IOException {
-		int mapSize = in.readInt();
-		for (int i = 0; i < mapSize; i++) {
-			wrdMap.put(in.readInt(), in.readInt());
-		}
-	}
-
+	@Override
 	public void write(DataOutput out) throws IOException {
-		Iterator iter = wrdMap.entrySet().iterator();
-		// write in total word
-		out.writeInt(wrdMap.size());
-		while (iter.hasNext()) {
-			Map.Entry<Integer, Integer> entry = (Map.Entry) iter.next();
-			// write in key value pairs as key then value
-			out.writeInt(entry.getKey());
-			out.writeInt(entry.getValue());
+		super.write(out);
 
-		}
+		out.writeInt(wordCount);
+	}
 
+	@SuppressWarnings("unchecked")
+	@Override
+	public void readFields(DataInput in) throws IOException {
+		super.readFields(in);
+
+		wordCount = in.readInt();
 	}
 
 }
