2cf024ffa70dab0e1a1e005ce7ebe9af59d2b416
ctsai <ctsai@netdb.cs.nthu.edu.tw>
2016-04-07 13:44:28 +0800

    Combine bug Fix (Combine and reduce sould have same input type)

--- hw1/src/main/java/CloudCompu/hw1/InvIdxCombi.java
+++ hw1/src/main/java/CloudCompu/hw1/InvIdxCombi.java
@@ -1,6 +1,7 @@
 package CloudCompu.hw1;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.MapWritable;
@@ -13,18 +14,27 @@ public class InvIdxCombi extends Reducer<Text, KeyDetial, Text, KeyDetial> {
 	private Text file = new Text();
 	private IntWritable count = new IntWritable();
 
-	public void reduce(Text key, Iterable<KeyDetial> values, Context context) throws IOException, InterruptedException {
-		int sum = 0;
-		for (KeyDetial val : values) {
-			sum += val.getWordCount();
+	public void reduce(Text key, Iterable<KeyDetial> values, Context context)
+			throws IOException, InterruptedException {
+
+		Iterator<KeyDetial> iter = values.iterator();
+		KeyDetial kdHead = iter.next();
+		if (kdHead.getWordCount() == 0) {
+			//Already Combined, Fast Pass
+			context.write(key, kdHead);
+		} else {
+			int sum = 0;
+			sum += kdHead.getWordCount();
+			while(iter.hasNext()){
+				sum += iter.next().getWordCount();
+			}
+			String[] fileWord = key.toString().split("_");
+			word.set(fileWord[0]);
+			file.set(fileWord[1]);
+			count.set(sum);
+
+			kd.put(file, count);
+			context.write(word, kd);
 		}
-		String[] fileWord = key.toString().split("_");
-		word.set(fileWord[0]);
-		file.set(fileWord[1]);
-		count.set(sum);
-
-		map.put(file, count);
-		context.write(word, map);
-
 	}
 }
