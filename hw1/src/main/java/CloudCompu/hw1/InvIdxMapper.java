3e83c3c648d5a92560916fdd58b54969cecbbb1c
ctsai <ctsai@netdb.cs.nthu.edu.tw>
2016-04-07 14:36:28 +0800

    Fix map report

--- hw1/src/main/java/CloudCompu/hw1/InvIdxMapper.java
+++ hw1/src/main/java/CloudCompu/hw1/InvIdxMapper.java
@@ -9,18 +9,24 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class InvIdxMapper extends Mapper<LongWritable, Text, Text, KeyDetial>{
+public class InvIdxMapper extends Mapper<LongWritable, Text, Text, KeyDetial> {
 	private KeyDetial one = new KeyDetial();
-	private Text  word = new Text(); 
-	public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-		FileSplit fileSplit = (FileSplit)context.getInputSplit();
+	private Text word = new Text();
+
+	public void map(LongWritable key, Text value, Context context)
+			throws IOException, InterruptedException {
+
+		FileSplit fileSplit = (FileSplit) context.getInputSplit();
 		String filename = fileSplit.getPath().getName();
 		StringTokenizer itr = new StringTokenizer(value.toString());
-		while(itr.hasMoreTokens()){
+		while (itr.hasMoreTokens()) {
 			String toProcess = itr.nextToken();
-			word.set(toProcess+"_"+filename);
-			one.setWordCount(1);
-			context.write(word, one);
+			if (Character.isAlphabetic(toProcess.toCharArray()[0])) {
+				word.set(toProcess + "_" + filename);
+				one.setWordCount(1);
+				context.write(word, one);
+			}
 		}
+
 	}
 }
--- hw1/src/main/java/CloudCompu/hw1/InvIdxReducer.java
+++ hw1/src/main/java/CloudCompu/hw1/InvIdxReducer.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.MapWritable;
@@ -23,15 +24,22 @@ public class InvIdxReducer extends Reducer<Text, KeyDetial, Text, Text> {
 				Map.Entry<Text, IntWritable> entry = (Map.Entry) iter.next();
 				tmp_word = entry.getKey().toString();
 				if (map.containsKey(tmp_word)) {
-					map.put(tmp_word, map.get(tmp_word) + entry.getValue().get());
+					map.put(tmp_word, map.get(tmp_word)
+							+ entry.getValue().get());
 				} else {
 					map.put(tmp_word, entry.getValue().get());
 				}
 
 			}
 		}
-
-		detail.set(map.toString());
-		context.write(key,detail);
+		String detString = "";
+		Iterator iter = map.entrySet().iterator();
+		while (iter.hasNext()) {
+			Map.Entry<String, Integer> entry = (Entry<String, Integer>) iter
+					.next();
+			detString.concat(entry.getKey() + ":" + entry.getValue());
+		}
+		detail.set(detString);
+		context.write(key, detail);
 	}
 }
