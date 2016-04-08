d735c9ae5f8bc313632224a1788211c08b69890f
ctsai <ctsai@netdb.cs.nthu.edu.tw>
2016-04-08 18:47:08 +0800

    Fix new type bug

--- hw1/src/main/java/CloudCompu/hw1/LongArrayWritable.java
+++ hw1/src/main/java/CloudCompu/hw1/LongArrayWritable.java
@@ -9,8 +9,13 @@ public class LongArrayWritable extends ArrayWritable {
 	public LongArrayWritable(Class<? extends Writable> valueClass) {
 		super(LongWritable.class);
 	}
-    public LongArrayWritable(LongWritable[] longs) {
-        super(LongWritable.class);
-        set(longs);
-    }
+
+	public LongArrayWritable(LongWritable[] longs) {
+		super(LongWritable.class);
+		LongWritable[] offset = new LongWritable[longs.length];
+		for (int i = 0; i < longs.length; i++) {
+			offset[i] = new LongWritable(longs[i].get());
+		}
+		set(offset);
+	}
 }
