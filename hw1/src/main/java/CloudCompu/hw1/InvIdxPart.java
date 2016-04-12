79481350bbb641729b256370c0ec9f1345d28dd4
ctsai <ctsai@netdb.cs.nthu.edu.tw>
2016-04-12 16:42:28 +0800

    Fix some bug in part

--- hw1/src/main/java/CloudCompu/hw1/InvIdxPart.java
+++ hw1/src/main/java/CloudCompu/hw1/InvIdxPart.java
@@ -4,13 +4,16 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Partitioner;
 
 public class InvIdxPart extends Partitioner<Text, LongArrayWritable> {
-	public int getPartition(Text key, LongArrayWritable value, int numReduceTasks) {
-		int c = key.charAt(0);
+	public int getPartition(Text key, LongArrayWritable value,
+			int numReduceTasks) {
+		double c = key.charAt(0);
+		double a = 'a';
+		double A = 'A';
 		int part = numReduceTasks / 2;
-		if (c < 'a') {
-			return part * ((c - 'A') / 26);
+		if (c < a) {
+			return (int) (part * ((c - A) / 26));
 		} else {
-			return part * ((c - 'a') / 26) + part;
+			return (int) (part * ((c - a) / 26) + part);
 		}
 
 	}
