9d675d50f2db2b2a0b9246432eaa6aa047fcdf05
ctsai <ctsai@netdb.cs.nthu.edu.tw>
2016-04-22 11:32:58 +0800

    Retrival output complete

--- hw1/src/main/java/CloudCompu/hw1/RetvalReducer.java
+++ hw1/src/main/java/CloudCompu/hw1/RetvalReducer.java
@@ -1,51 +1,41 @@
 package CloudCompu.hw1;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.SortedSet;
-import java.util.TreeSet;
-
-import org.apache.hadoop.io.ArrayWritable;
-import org.apache.hadoop.io.DoubleWritable;
-import org.apache.hadoop.io.MapWritable;
+import java.nio.charset.Charset;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.mapreduce.Reducer;
 
 public class RetvalReducer extends Reducer<Text, WordPos, Text, Text> {
 	private Text detail = new Text();
 
-	public void reduce(Text key, Iterable<WordPos> values, Context context)
-			throws IOException, InterruptedException {
+	public void reduce(Text key, Iterable<WordPos> values, Context context) throws IOException, InterruptedException {
+		Configuration conf = new Configuration();
+		FileSystem fs = FileSystem.get(conf);
+		String inputDir = context.getConfiguration().get("inputDir");
+		FileStatus[] status_list = fs.listStatus(new Path(inputDir));
 
 		int reducerId = context.getTaskAttemptID().getTaskID().getId();
 		String detString = "";
-		
+		int file_id;
+		int subRank = 0;
+		Path inFile;
+		byte[] buffer = new byte[1024];
 		for (WordPos val : values) {
-			detString = String detString + "Rank "+
-		}
-
-		String detString = tmpMap.size() + " > ";
-		// Get number of files for further usage
-		long total_word = context.getCounter(
-				"org.apache.hadoop.mapred.Task$Counter", "MAP_INPUT_RECORDS")
-				.getValue();
-
-		SortedSet<String> keys = new TreeSet<String>(tmpMap.keySet());
-		for (String file : keys) {
-			DoubleWritable[] l = tmpMap.get(file);
-			detString = detString + " " + total_word + " " + file + " "
-					+ l.length + " " + l[l.length - 1].get() + " [";
-			Arrays.sort(l);
-			for (int i = 0; i < l.length - 1; i++) {
-				detString = detString + l[i].get() + ",";
+			file_id = Integer.parseInt(key.toString().split("_")[0]);
+			detString = detString + "Rank " + subRank + reducerId + ": " + status_list[file_id] + " score = "
+					+ Double.toString(val.getW()) + "\r\n";
+			detString = detString + "************************\r\n";
+			subRank++;
+			inFile = new Path(inputDir + "/" + status_list[file_id]);
+			for (String o : val.toString().split(" ")) {
+				fs.open(inFile).read(Long.parseLong(o) - 50L, buffer, 0, 1024);
+				detString = detString + new String(buffer, Charset.forName("UTF-8"));
 			}
-
-			detString = detString + "]; ";
+			detString = detString + "************************\r\n";
 		}
 
 		detail.set(detString);
