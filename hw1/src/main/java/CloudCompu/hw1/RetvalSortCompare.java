d3f9ff6ce0a584262d122c4de3246848d0473f47
ctsai <ctsai@netdb.cs.nthu.edu.tw>
2016-04-23 14:50:54 +0800

    Try new Reducw

--- hw1/src/main/java/CloudCompu/hw1/RetvalSortCompare.java
+++ hw1/src/main/java/CloudCompu/hw1/RetvalSortCompare.java
@@ -15,11 +15,13 @@ public class RetvalSortCompare extends WritableComparator {
 		String[] s1, s2;
 		s1 = t1.toString().split("_");
 		s2 = t2.toString().split("_");
-		int compare = s1[0].compareTo(s2[0]);
+		return Double.compare(Double.parseDouble(t1.toString()),
+				Double.parseDouble(t2.toString()));
+		/*int compare = s1[0].compareTo(s2[0]);
 		if (compare == 0) {
 			return Double.compare(Double.parseDouble(s1[1]),
 					Double.parseDouble(s2[1]));
 		} 
-		return compare;
+		return compare;*/
 	}
 }
