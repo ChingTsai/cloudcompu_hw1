be874783dbea63fc30bce2cd391d14fb36c9a266
ctsai <ctsai@netdb.cs.nthu.edu.tw>
2016-04-23 20:50:02 +0800

    [Two Phase] Testing 2nd Phase

--- hw1/src/main/java/CloudCompu/hw1/Retval2ndMapper.java
+++ hw1/src/main/java/CloudCompu/hw1/Retval2ndMapper.java
@@ -1,6 +1,5 @@
 package CloudCompu.hw1;
 
-
 import java.io.IOException;
 import java.util.StringTokenizer;
 
@@ -8,16 +7,17 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Mapper.Context;
 
-public class Retval2ndMapper extends Mapper<Text, Text, Text, WordPos>{
+public class Retval2ndMapper extends Mapper<Text, Text, Text, WordPos> {
 	private WordPos KeyWeight = new WordPos();
 
 	public void map(Text key, Text value, Context context) throws IOException,
 			InterruptedException {
 		String[] s = value.toString().split("~");
-		KeyWeight.set(s[1]);
-		KeyWeight.setW(Double.parseDouble(key.toString()));
-		KeyWeight.setfile_id(Integer.parseInt(s[0]));
-		context.write(key, KeyWeight);
-
+		if (s.length > 1) {
+			KeyWeight.set(s[1]);
+			KeyWeight.setW(Double.parseDouble(key.toString()));
+			KeyWeight.setfile_id(Integer.parseInt(s[0]));
+			context.write(key, KeyWeight);
+		}
 	}
 }
