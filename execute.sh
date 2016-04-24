329f12f021051fbf56c357ed31b5179ad3bf8d5c
ChingTsai <ctsai@netdb.cs.nthu.edu.tw>
2016-04-24 11:35:36 +0800

    New Directory, New scripty

--- execute.sh
+++ execute.sh
@@ -4,6 +4,10 @@
 #hadoop jar WordCount.jar wordcount.WordCount /user/shared/WordCount/Input /user/TA/WordCount/Output
 #hdfs dfs -cat /user/TA/WordCount/Output/part-*
 
+hdfs dfs -rm -r input
+hdfs dfs -put input input
 hdfs dfs -rm -r invidx
 hadoop jar ${PWD##*/}.jar CloudCompu.hw1.InvertedIndex input invidx
 hdfs dfs -cat invidx/part-*
+rm -rf invidx
+hdfs dfs -get invidx .
--- retrival.sh
+++ retrival.sh
@@ -4,7 +4,11 @@
 #hadoop jar WordCount.jar wordcount.WordCount /user/shared/WordCount/Input /user/TA/WordCount/Output
 #hdfs dfs -cat /user/TA/WordCount/Output/part-*
 
+
 hdfs dfs -rm -r output
 hdfs dfs -rm -r tmp
 hadoop jar ${PWD##*/}.jar CloudCompu.hw1.Retrieval invidx input output "catch bag" 1 
 hdfs dfs -cat output/part-*
+rm -rf output
+hdfs dfs -get output .
+mv output/part-r-00000 output/output_retrieval.txt
